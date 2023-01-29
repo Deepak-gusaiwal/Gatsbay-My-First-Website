9d38790bab24301356c5bcc5c6120d42ec0b9fb9
Deepak <dgusaiwal537@Gmail.com>
2023-01-29 14:18:30 +0530

    third commite update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import {footer} from '../components/style.module.css'
 const Footer = () => {
   return (
         <div className={footer}>
-            {new Date().getFullYear()} All @copyrights reserved by Deepak
+            {new Date().getFullYear()} All @copyrights reserved by Deepak Gusaiwal
         </div>
   )
 }
